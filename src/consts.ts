b6d923f771d51850fd72c6756b42791016801029
Hektor Peña <dev@hektor.mx>
2026-01-01 17:37:45 -0600

    Agregar logo y redes sociales al footer, secciÃ³n de patrocinadores y ajustes de tÃ­tulo

--- src/consts.ts
+++ src/consts.ts
@@ -1,7 +1,10 @@
-export const SITE_TITLE = 'Hektor';
+export const SITE_TITLE = 'Gigantes de Aguascalientes';
 export const SITE_DESCRIPTION = '';
-export const SITE_SINCE = '2000';
+export const SITE_SINCE = '2026';
 
 export const patrocinadores = [
-    { img: "patrocinador.svg", url: "https://www.hektor.mx/" },
+    { img: "patrocinadores/victoria.svg", url: "https://www.victoria.com.mx/" },
+    { img: "patrocinadores/tecate.svg", url: "https://www.tecate.com.mx/" },
+    { img: "patrocinadores/banorte.svg", url: "https://www.banorte.com/" },
+    { img: "patrocinadores/coca-cola.svg", url: "https://www.coca-cola.com.mx/" },
   ];
