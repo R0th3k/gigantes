b653b36138cb9b4b83e88fd405fb42899625514e
Hektor Peña <dev@hektor.mx>
2026-01-20 19:45:22 -0600

    Actualizar tabla de puntajes, nuevo post de blog y ajustar columnas en resultados

--- src/consts.ts
+++ src/consts.ts
@@ -1,22 +1,22 @@
 export const SITE_TITLE = 'Gigantes de Aguascalientes';
-export const SITE_DESCRIPTION = '';
+export const SITE_DESCRIPTION = 'Primer equipo profesional de voleibol en Aguascalientes 🏐🔥';
 export const SITE_SINCE = '2026';
 
 export const patrocinadores = [
-    { img: "patrocinadores/p-1.png", url: "#" },
-    { img: "patrocinadores/p-2.png", url: "#" },
-    { img: "patrocinadores/p-3.png", url: "#" },
-    { img: "patrocinadores/p-4.png", url: "#" },
-    { img: "patrocinadores/p-5.png", url: "#" },
-    { img: "patrocinadores/p-6.png", url: "#" },
-    { img: "patrocinadores/p-7.png", url: "#" },
-    { img: "patrocinadores/p-8.png", url: "#" },
-    { img: "patrocinadores/p-9.png", url: "#" },
-    { img: "patrocinadores/p-10.png", url: "#" },
-    { img: "patrocinadores/p-11.png", url: "#" },
-    { img: "patrocinadores/p-12.png", url: "#" },
-    { img: "patrocinadores/p-13.png", url: "#" },
-    { img: "patrocinadores/p-14.png", url: "#" },
-    { img: "patrocinadores/p-15.png", url: "#" },
-    { img: "patrocinadores/p-16.png", url: "#" },
+    { img: "patrocinadores/p-1.png", url: "https://www.facebook.com/QASmexico/?locale=es_LA" },
+    { img: "patrocinadores/p-2.png", url: "https://www.instagram.com/bm.boutiquecreativo/?hl=es" },
+    { img: "patrocinadores/p-3.png", url: "https://aguascalientes.gob.mx/" },
+    { img: "patrocinadores/p-4.png", url: "https://aguascalientes.gob.mx/" },
+    { img: "patrocinadores/p-5.png", url: "https://www.lanqgel.com/" },
+    { img: "patrocinadores/p-6.png", url: "https://www.ags.gob.mx/" },
+    { img: "patrocinadores/p-7.png", url: "https://www.calvillo.gob.mx/" },
+    { img: "patrocinadores/p-8.png", url: "https://molten.com.mx/" },
+    { img: "patrocinadores/p-9.png", url: "https://www.facebook.com/UdiSportOficial/?locale=es_LA" },
+    { img: "patrocinadores/p-10.png", url: "https://electrolit.com.mx/" },
+    { img: "patrocinadores/p-11.png", url: "https://www.fivb.com/es/" },
+    { img: "patrocinadores/p-12.png", url: "https://www.lvpmexico.com/?utm_content=link_in_bio&fbclid=PAZXh0bgNhZW0CMTEAc3J0YwZhcHBfaWQMMjU2MjgxMDQwNTU4AAGnpKJ30jiryrFOA3XaEFZ80_0NXHfUtz9EMyhqU6P7r3FwUVY3Csyi09w0d7o_aem_R3GxhDSHPrDXePujPgijHw" },
+    { img: "patrocinadores/p-13.png", url: "https://www.instagram.com/pixeledmx/" },
+    { img: "patrocinadores/p-14.png", url: "https://fitsi.com.mx/clubes/aguascalientes" },
+    { img: "patrocinadores/p-15.png", url: "https://centromedicolasalud.com/" },
+    { img: "patrocinadores/p-16.png", url: "https://redcolacentro.mx/" },
   ];
