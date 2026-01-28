93d1646dc7633d02d5b368ef1846739f68401695
Hektor Peña <dev@hektor.mx>
2026-01-27 21:36:52 -0600

    Agregar YouTube y eliminar X de redes sociales, actualizar tabla de puntajes

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://hektor.mx/',
+  site: 'https://gigantes.mx/',
   base: '/',
   integrations: [mdx(), vue(), sitemap()],
 });
